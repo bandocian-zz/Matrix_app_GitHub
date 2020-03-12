bb0e231bc2ebb887bf27d2b9e24889026a15f5ca
Michal Sičák <michal.sicak@ness.com>
2020-03-12 09:21:22 +0100

    securing my own token more

--- githubAPI/src/main/kotlin/githubapi/Config.kt
+++ githubAPI/src/main/kotlin/githubapi/Config.kt
@@ -10,6 +10,6 @@ class Config{
     fun authUser(): String = "bandocian"
 
     @Bean
-    fun authPassword(): String = "990352a71d246c9b45f941fa73078f7011075afe"
+    fun authPassword(): String = "8fb2bf43f219c6dbb5695481aad1d32530143b94"
 
 }
